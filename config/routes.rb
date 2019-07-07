6ff3521838ac88487826dce33256ec26e74d4814
Rodrigo <fuentesosorio@fuentesosorio.com>
2019-07-06 20:18:19 -0400

    Uploading Sass_demo project

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+  get 'products/home'
+  get 'products/about'
+  resources :products
   get 'landing/index'
+
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
